055d442ad3e58d0a85d3df8ba2e91c9928217820
istrybuk <79363807+istrybuk@users.noreply.github.com>
2023-09-08 21:09:47 +0300

    add lesson1

---
+++ Homework/Lesson_1/show_all.sh
@@ -0,0 +1,17 @@
+!/bin/bash
+# выход в корень
+cd 
+# переход в папку
+cd GitHub/Ksendzov/Homework/Lesson_1/
+# Создать 3 папки
+mkdir NAME{1..3}
+# зайти в любую папку
+cd NAME2
+touch text{1..3}.txt
+touch json{1..2}.json
+mkdir fol{1..3}
+ls
+mv json{1..2}.json fol1/
+ls
+ls fol1/
+
